31ce485c39e5555145bb42e1f0156476d5a8b183
Romain Quilliot <romain.addweb@gmail.com>
2015-02-22 21:28:58 +0100

    Ajout du choix du personnage

--- app_dev/libraries/controllers/gameCharactersPageController.js
+++ app_dev/libraries/controllers/gameCharactersPageController.js
@@ -2,5 +2,14 @@ var gameCharactersPageController = {};
 
 
 gameCharactersPageController.launch = function () {
-    console.log('Characters');
+    $('#game-content').on('click', '#select-character li', gameCharactersPageController.selectChar);
+};
+
+
+gameCharactersPageController.selectChar = function () {
+    var character = $(this).attr('data-char');
+    
+    pageController
+      .setParams({ character: character })
+      .display('cells');
 };
