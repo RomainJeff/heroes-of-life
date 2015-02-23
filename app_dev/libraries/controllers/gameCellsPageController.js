e6d30c1d8f2946c50b95e96973884dbad02564a5
Romain Quilliot <romain.addweb@gmail.com>
2015-02-23 01:54:02 +0100

    Mise a jour du design du choix des cellules

--- app_dev/libraries/controllers/gameCellsPageController.js
+++ app_dev/libraries/controllers/gameCellsPageController.js
@@ -1,9 +1,12 @@
 var gameCellsPageController = {
-    cells: 1,
-    grille: null
+    cells: 25,
+    grille: null,
+    counter : null,
+    timeLeft: 240
 };
 
 
+// Fonction executee au lancement de la page
 gameCellsPageController.launch = function () {
     var character = $('#character-saved').html();
     gameCellsPageController.grille = new grilleController();
@@ -13,12 +16,36 @@ gameCellsPageController.launch = function () {
             .draw('grille-cells');
 
     gameCellsPageController.refreshCells();
+    gameCellsPageController.counter = setInterval(gameCellsPageController.refreshCounter, 1000);
 
     $('#game-content').on('click', '#grille-cells .line .row', gameCellsPageController.cellsSelect);
     $('#game-content').on('click', '#start-game', gameCellsPageController.startGame);
 };
 
 
+// Rafraichie le decompte
+gameCellsPageController.refreshCounter = function () {
+    gameCellsPageController.timeLeft--;
+
+    var timeleft = gameCellsPageController.timeLeft;
+    var minutes = Math.floor(timeleft / 60);
+    var seconds = timeleft - minutes * 60;
+
+    if (timeleft <= 0) return gameCellsPageController.callbackCounter();
+
+    $('#time-counter').html(minutes +":"+ seconds);
+};
+
+
+// Quand le compteur est over
+gameCellsPageController.callbackCounter = function () {
+    clearInterval(this.counter);
+
+    this.startGame();
+};
+
+
+// Quand l'utilisateur clique sur "commencer le jeu"
 gameCellsPageController.startGame = function () {
     pageController
         .setParams({title: 'Waiting for your mate'})
@@ -26,6 +53,7 @@ gameCellsPageController.startGame = function () {
 };
 
 
+// Quand l'utilisateur selectionne une cellule
 gameCellsPageController.cellsSelect = function () {
     var isActive    = $(this).attr('data-active');
     var lineCoord   = $(this).parent().attr('id').replace('line-', '');
@@ -44,24 +72,16 @@ gameCellsPageController.cellsSelect = function () {
     }
 
     gameCellsPageController.refreshCells();
-    gameCellsPageController.verifyStartButton();
 };
 
 
+// Verifie que l'utilisateur a encore des ressources
 gameCellsPageController.hasRessources = function () {
     return (this.cells > 0) ? true : false;
 };
 
 
+// Rafraichie le nombre de cellules restantes
 gameCellsPageController.refreshCells = function () {
     $('#cells-counter i').html(this.cells);
-};
-
-
-gameCellsPageController.verifyStartButton = function () {
-    if (!this.hasRessources()) {
-        $('#start-game').removeClass('blocked');
-    } else {
-        $('#start-game').addClass('blocked');
-    }
 };
