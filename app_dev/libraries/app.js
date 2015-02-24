d8264f99bcaf1ec8e29c5cd0b7fb789152ba545a
Romain Quilliot <romain.addweb@gmail.com>
2015-02-24 01:52:26 +0100

    Ajout des evenements

--- app_dev/libraries/app.js
+++ app_dev/libraries/app.js
@@ -1,5 +1,16 @@
+// SOCKETS
+var socketInterface = new socketController('localhost:8080');
+
+socketInterface.listen({
+    'user:canPlay':         gameHomePageController.eventCanPlay,
+    'character:isValid':    gameCharactersPageController.eventIsValid,
+    'user:logout':          gameHomePageController.eventLogout
+});
+
+
 // HOMEPAGE
 indexController.home();
+
 
 // ROUTES
 routingController.addRoute('home', gameHomePageController.launch);
