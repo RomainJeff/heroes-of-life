30960023ea0b47ae32af7d600ec28b0b9c4e4c9b
Romain Quilliot <romain.addweb@gmail.com>
2015-03-25 18:43:36 +0100

    Ajout du tutoriel

--- app_dev/libraries/controllers/gameCellsPageController.js
+++ app_dev/libraries/controllers/gameCellsPageController.js
@@ -26,10 +26,19 @@ gameCellsPageController.launch = function () {
             .draw('grille-cells');
 
     gameCellsPageController.refreshCells();
-    gameCellsPageController.counter = setInterval(gameCellsPageController.refreshCounter, 1000);
 
     $('#game-content').on('click', '#grille-cells .line .row', gameCellsPageController.cellsSelect);
     $('#game-content').on('click', '#start-game', gameCellsPageController.startGame);
+
+    // Event listener tutoriel
+    $('#tutorial-content [data-close="true"]').on('click', gameCellsPageController.tutorialClose);
+    $('#tutorial-content [data-start="true"]').on('click', gameCellsPageController.tutorialStart);
+
+    // Affichage du tutoriel
+    setTimeout(function () {
+        $('#tutorial-overlay').addClass('active');
+        $('#tutorial-content').addClass('active');
+    }, 150);
 };
 
 
@@ -95,6 +104,96 @@ gameCellsPageController.hasRessources = function () {
 // Rafraichie le nombre de cellules restantes
 gameCellsPageController.refreshCells = function () {
     $('#cells-counter i').html(this.cells);
+};
+
+
+// Ferme le tutoriel
+gameCellsPageController.tutorialClose = function () {
+    $('#tutorial-content').removeClass('active');
+    $('#tutorial-overlay').removeClass('active');
+
+    // On lance le decompte
+    gameCellsPageController.counter = setInterval(gameCellsPageController.refreshCounter, 1000);
+};
+
+
+// Commence le tutoriel
+gameCellsPageController.tutorialStart = function () {
+    // On affiche les fleches
+    $('#tutorial-content .arrow').addClass('active');
+
+    // On tourne la slide
+    gameCellsPageController.tutorialSlide("+");
+
+    // On ecoute l'event clavier
+    $(document).on('keyup', gameCellsPageController.tutorialKeyboardListener);
+    $('#tutorial-content .arrow').on('click', gameCellsPageController.tutorialArrowListener);
+};
+
+
+// Change de slide
+gameCellsPageController.tutorialSlide = function (signe) {
+    var currentSlide = $('.slide.active');
+    var slide = currentSlide.attr('data-slide');
+    var slidesLength = $('.slide').length;
+
+    if (signe == "+") {
+        slide++;
+    } else {
+        slide--;
+    }
+
+    // Si il y a pas de slide suivante
+    if (slide < 0 || slide >= slidesLength) {
+        return false;
+    }
+
+    var nextSlide = $('.slide[data-slide="'+ slide +'"]');
+
+    // On verifie si il faut cacher les fleches
+    if (slide <= 0) {
+        $('.arrow.arrow-left').removeClass('active');
+    } else {
+        $('.arrow.arrow-left').addClass('active');
+    }
+
+    if (slide >= (slidesLength - 1)) {
+        $('.arrow.arrow-right').removeClass('active');
+    } else {
+        $('.arrow.arrow-right').addClass('active');
+    }
+
+
+    // On check le signe pour appliquer la bonne anim
+    if (signe == "+") {
+        currentSlide.addClass('left').removeClass('active');
+        nextSlide.addClass('active').removeClass('right');
+    } else {
+        currentSlide.addClass('right').removeClass('active');
+        nextSlide.addClass('active').removeClass('left');
+    }
+};
+
+
+// Change le slide quand on clique sur une des fleches visuelles
+gameCellsPageController.tutorialArrowListener = function () {
+    if ($(this).hasClass('arrow-right')) {
+        gameCellsPageController.tutorialSlide('+');
+    } else {
+        gameCellsPageController.tutorialSlide('-');
+    }
+};
+
+
+// Change le slide quand on clique sur les fleches du clavier
+gameCellsPageController.tutorialKeyboardListener = function (e) {
+    var keyCode = e.keyCode;
+
+    if (keyCode == 39) {
+        gameCellsPageController.tutorialSlide('+');
+    } else if (keyCode == 37) {
+        gameCellsPageController.tutorialSlide('-');
+    }
 };
 
 
